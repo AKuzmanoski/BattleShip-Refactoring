e33d810fb9044c4ae5485b8f499e6cf4073d458d
Angela Josifovska <josifovska.angela@students.finki.ukim.mk>
2015-01-12 22:09:36 +0100

    .

---
+++ BattleShipsRefactoring[TeamWork]/src/Battleships/AttackMousePressListener.java
@@ -0,0 +1,49 @@
+package Battleships;
+
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import Battleships.Graphics.AttackPanel;
+
+public class AttackMousePressListener extends MouseAdapter{
+	private AttackPanel a;
+	private GUI gui;
+	private AxisResolver resolver;
+	
+	public AttackMousePressListener(AttackPanel p, GUI gui2)
+	{
+		a=p;
+		gui=gui2;
+		resolver=new AxisResolver();
+	}
+	  
+	
+			public void mousePressed(MouseEvent event)
+			{
+				if(gui.data.gameState.IsAcceptingPlayerInput())
+				{
+					Graphics g = a.getGraphics();
+					int x = event.getX();
+					int y = event.getY();
+				
+					int gridj= resolver.resolveAxisCoOrdinate(x);
+					int gridi= resolver.resolveAxisCoOrdinate(y);
+				
+					Graphics attackPanelGraphics = a.getGraphics();
+					
+	                String acceptPlayerShotString = 
+	                	gui.data.gameState.acceptPlayerShot(gridi,gridj, attackPanelGraphics, gui.data.outText);
+	                
+	                //gui.gameState.updatePlayerClick(gridi, gridj, gui);
+	                
+	                
+	                
+					System.out.println(acceptPlayerShotString);
+					System.out.println("Element corresponds to " + gridi + gridj);
+					
+					
+				}
+			}
+
+}
---
+++ BattleShipsRefactoring[TeamWork]/src/Battleships/HomeMousePressListener.java
@@ -0,0 +1,52 @@
+package Battleships;
+
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import Battleships.Graphics.HomePanel;
+
+public class HomeMousePressListener extends MouseAdapter
+{
+	
+	private HomePanel a;
+	private GUI gui;
+	private AxisResolver resolver;
+	
+	public HomeMousePressListener(HomePanel p, GUI gui2)
+	{
+		a=p;
+		gui=gui2;
+		resolver=new AxisResolver();
+	}
+	
+	
+			public void mousePressed(MouseEvent event)
+			{
+				
+				Graphics g = a.getGraphics();
+				
+				int x = event.getX();
+				int y = event.getY();
+			
+				int gridj= resolver.resolveAxisCoOrdinate(x);
+				int gridi= resolver.resolveAxisCoOrdinate(y);
+				
+				//gui.
+				
+				//gui.data.gameState.deployVessel(gridi, gridj);
+				//System.out.println(gui.data.gameState.playerDeployStateToString();
+				
+				if(!gui.data.gameState.isBothPlayerAndAgentShipsDeployed())
+				{
+					gui.deploy(gridi,gridj);
+					
+				}
+				System.out.println("Element corresponds to " + gridi + gridj);
+				//repaint();
+			}
+		
+
+}
+
+	
